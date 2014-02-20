84beac5cdfaaf9641ab66ad0a099670dbbbba44b
Miroslav Jiřík <miroslav.jirik@gmail.com>
2014-02-20 17:30:05 +0100

    pep8

--- tests/pycut_test.py
+++ tests/pycut_test.py
@@ -1,12 +1,9 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-
 # import funkcí z jiného adresáře
 import sys
 import os.path
-import copy
 
 path_to_script = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(path_to_script, "../src/"))
@@ -15,7 +12,6 @@ import unittest
 import numpy as np
 
 import pycut
-
 
 
 class PycutTest(unittest.TestCase):
@@ -27,11 +23,9 @@ class PycutTest(unittest.TestCase):
         """
         test of pycut.__ordered_values_by_indexes
         """
-        slab = {'none':0, 'liver':1, 'porta':2, 'lesions':6}
-        voxelsize_mm = np.array([1.0,1.0,1.2])
 
         # there must be some data
-        img = np.zeros([32,32,32], dtype=np.int16)
+        img = np.zeros([32, 32, 32], dtype=np.int16)
         data = np.array([
             [0, 1, 1],
             [0, 2, 2],
@@ -47,7 +41,6 @@ class PycutTest(unittest.TestCase):
         vals = gc._ImageGraphCut__ordered_values_by_indexes(data, inds)
         expected = np.array([0, 1, 1, 0, 2, 0])
         self.assertItemsEqual(vals, expected)
-
 
 
 if __name__ == "__main__":
