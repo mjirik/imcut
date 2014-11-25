2da86d09fa6ca613d37508c74ccb6fef535e7ad9
M. Jirik <miroslav.jirik@gmail.com>
2014-11-25 19:33:36 +0100

    version number updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.10',
+    version='1.0.11',
     url='https://github.com/mjirik/pyseg_base',
     author='Miroslav Jirik and Vladimir Lukes',
     author_email='miroslav.jirik@gmail.com',
