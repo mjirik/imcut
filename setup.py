07d91621d49b607c5764b289d840598a7f5df8c8
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-11-05 12:51:25 +0100

    pyqt removed

--- setup.py
+++ setup.py
@@ -55,10 +55,13 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    # sdf
-    install_requires=['numpy', 'scipy', 'pyqt'],
-    # potiz
+    install_requires=['numpy', 'scipy' ],
+    # 'SimpleITK'],  # Removed becaouse of errors when pip is installing
     dependency_links=[],
+    # sdf
+    # install_requires=['numpy', 'scipy', 'pyqt'],
+    # potiz
+    # dependency_links=[],
 
     # dependency_links=['https://github.com/mjirik/gco_python'],
 
