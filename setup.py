6e6ee5a3b0aecba4e3380cc72b774778404d96e8
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-11-05 12:54:29 +0100

    conda build fixed

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['numpy', 'scipy' ],
+    install_requires=['numpy', 'scipy', 'pydicom'],
     # 'SimpleITK'],  # Removed becaouse of errors when pip is installing
     dependency_links=[],
     # sdf
