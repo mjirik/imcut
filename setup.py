5d41f64db7ab03f213f9e3eb96bdd1496f023a78
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-11-05 12:33:58 +0100

    new version

--- setup.py
+++ setup.py
@@ -55,8 +55,11 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    # sdf
     install_requires=['numpy', 'scipy', 'pyqt'],
+    # potiz
     dependency_links=[],
+
     # dependency_links=['https://github.com/mjirik/gco_python'],
 
     # If there are data files included in your packages that need to be
