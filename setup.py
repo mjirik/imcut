26f9e6638dc4e9df58f1a329c14e0e9ba432c24f
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-03-19 14:42:29 +0100

    same requirements in meta and requirements_conda

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['numpy', 'scipy', 'pydicom'],
+    install_requires=['numpy', 'scipy', 'pydicom', "scikit-learn"],
     # 'SimpleITK'],  # Removed becaouse of errors when pip is installing
     dependency_links=[],
     # sdf
