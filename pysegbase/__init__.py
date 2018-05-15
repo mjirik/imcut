b5520d5b0a120f2a2d3f61c31eb26defd89703e0
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-05-15 18:35:54 +0200

    fix versions

--- pysegbase/__init__.py
+++ pysegbase/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 __all__ = ['pycut',
            # 'seed_editor_qt',
            'dcmreaddata']
-__version__ = "1.2.39"
+__version__ = "1.2.40"
 # TODO remove QTSeedEditor from pysegbase
 # from .pycut import Model, ImageGraphCut
 # from .seed_editor_qt import QTSeedEditor
