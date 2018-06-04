af4751d27e62bb2f96d8ef24a01094449cbc85c2
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-06-04 13:57:47 +0200

    dcmreaddata test

---
+++ tests/dcmreaddata_test.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+# import funkcí z jiného adresáře
+import sys
+import os.path
+import unittest
+import scipy
+import numpy as np
+import pysegbase.dcmreaddata
+
+import logging
+logger = logging.getLogger(__name__)
+
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(path_to_script, "../src/"))
+
+from nose.plugins.attrib import attr
+from nose.tools import raises
+from pysegbase import graph
+
+class DcmReadDataTest(unittest.TestCase):
+    # @classmethod
+    # def setUpClass(cls):
+    #     if sys.version_info.major < 3:
+    #         cls.assertCountEqual = cls.assertItemsEqual
+    def test_dcmread(self):
+        pass
