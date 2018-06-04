a8bb894de3ec38c09bea3892667200d613bb0088
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-06-04 16:48:47 +0200

    no new unique values after resize

--- tests/image_manipulation_test.py
+++ tests/image_manipulation_test.py
@@ -26,13 +26,23 @@ class ImageManipulationTest(unittest.TestCase):
     # def setUpClass(cls):
     #     if sys.version_info.major < 3:
     #         cls.assertCountEqual = cls.assertItemsEqual
-    def test_imma(self):
+    def test_resize_to_shape_no_new_unique_values(self):
         data = np.zeros([10, 15, 12])
-        data[:5, :5, :7] = 1
+        value1 = 1
+        value2 = 2
+        data[:5, :7, :6] = value1
+        data[-5:, :7, :6] = value2
 
         expected_shape = [15, 15, 15]
         resized = imma.resize_to_shape(data, expected_shape)
+        unique = np.unique(resized)
 
         self.assertEqual(resized.shape[0], expected_shape[0])
         self.assertEqual(resized.shape[1], expected_shape[1])
         self.assertEqual(resized.shape[2], expected_shape[2])
+        self.assertEqual(resized[1, 1, 1], value1)
+        self.assertEqual(resized[-2, 1, 1], value2)
+        self.assertEqual(len(unique), 3)
+        self.assertEqual(unique[0], 0)
+        self.assertEqual(unique[1], 1)
+        self.assertEqual(unique[2], 2)
