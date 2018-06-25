e20eb450b44b91b5b24d43c71bb28ce9c97bfa4f
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-06-25 22:07:50 +0200

    Bump version: 1.2.50 → 1.2.51

--- imcut/__init__.py
+++ imcut/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 __all__ = ['pycut',
            # 'seed_editor_qt',
            'dcmreaddata']
-__version__ = "1.2.50"
+__version__ = "1.2.51"
 # DeprecationWarning("imcut package is renamed to imcut")
 # from .pycut import Model, ImageGraphCut
 # from .seed_editor_qt import QTSeedEditor
